e1fadf16177f1c41440b8972d449dcf8095662aa
yuki <yuki.onishi1129@gmail.com>
2022-04-16 06:08:28 +0900

    テスト修正

--- app/service/service_test.go
+++ app/service/service_test.go
@@ -50,10 +50,14 @@ func beforeAll() {
 	_, fileName, _, _ := runtime.Caller(0)
 	err = connectDB()
 	if err != nil {
+		// コンテナ停止
+		closeContainer()
 		log.Fatalf("db connect error: %v", err)
 	}
 	for _, sqlFileName := range createTableSQLFileName {
 		if err = execSQLScript(fmt.Sprintf("%s/../testdata/sql/create/%s.sql", filepath.Dir(fileName), sqlFileName)); err != nil {
+			// コンテナ停止
+			closeContainer()
 			log.Fatalf("%s, %v", fileName, err)
 		}
 	}
@@ -64,20 +68,28 @@ func beforeEach() {
 	_, fileName, _, _ := runtime.Caller(0)
 	// 外部キーを無効化
 	if err = execSQLScript(fmt.Sprintf("%s/../testdata/sql/fkkey/%s.sql", filepath.Dir(fileName), unEnabledFkKeySQLFileName)); err != nil {
-		log.Fatalf("%s, %v", fileName, err)
+		// コンテナ停止
+		closeContainer()
+		log.Fatalf("unenable foreignkey error: %s, %v", fileName, err)
 	}
 	// データ削除
 	for _, sqlFileName := range dropTableSQLFileName {
 		if err = execSQLScript(fmt.Sprintf("%s/../testdata/sql/truncate/%s.sql", filepath.Dir(fileName), sqlFileName)); err != nil {
-			log.Fatalf("%s, %v", fileName, err)
+			// コンテナ停止
+			closeContainer()
+			log.Fatalf("truncate table error: %s, %v", fileName, err)
 		}
 	}
 	// 外部キーを有効化
 	if err = execSQLScript(fmt.Sprintf("%s/../testdata/sql/fkkey/%s.sql", filepath.Dir(fileName), enabledFkKeySQLFileName)); err != nil {
-		log.Fatalf("%s, %v", fileName, err)
+		// コンテナ停止
+		closeContainer()
+		log.Fatalf("enable foreignkey error: %s, %v", fileName, err)
 	}
 	// テストデータ作成
 	if err = createTestData(); err != nil {
+		// コンテナ停止
+		closeContainer()
 		return
 	}
 }
@@ -91,12 +103,14 @@ func afterAll() {
 func createContainer() {
 	var err error
 	// 絶対パスを取得
-	_, fileName, _, _ := runtime.Caller(0)
+	//_, fileName, _, _ := runtime.Caller(0)
 
 	// Dockerとの接続
 	poolTest, err = dockertest.NewPool("")
 	poolTest.MaxWait = time.Minute * 2
 	if err != nil {
+		// コンテナ停止
+		closeContainer()
 		log.Fatalf("Could not connect to docker: %s", err)
 	}
 
@@ -107,6 +121,7 @@ func createContainer() {
 		Env: []string{
 			"POSTGRES_USER=postgres",
 			"POSTGRES_PASSWORD=secret",
+			"POSTGRES_DB=GO_POSTGRES_GRAPHQL_DB",
 		},
 		ExposedPorts: []string{"5432"},
 		PortBindings: map[docker.Port][]docker.PortBinding{
@@ -114,9 +129,9 @@ func createContainer() {
 				{HostIP: "0.0.0.0", HostPort: "5433"},
 			},
 		},
-		Mounts: []string{
-			fmt.Sprintf("%s/../../postgres/db:/docker-entrypoint-initdb.d", filepath.Dir(fileName)), // コンテナ起動時に実行するSQL
-		},
+		//Mounts: []string{
+		//	fmt.Sprintf("%s/../../postgres/db:/docker-entrypoint-initdb.d", filepath.Dir(fileName)), // コンテナ起動時に実行するSQL
+		//},
 		//Cmd: []string{
 		//	"mysqld",
 		//	"--character-set-server=utf8mb4",
@@ -127,6 +142,8 @@ func createContainer() {
 	// コンテナを起動
 	resourceTest, err = poolTest.RunWithOptions(runOptions)
 	if err != nil {
+		// コンテナ停止
+		closeContainer()
 		log.Fatalf("Could not start resource: %s", err)
 	}
 }
@@ -146,12 +163,14 @@ func connectDB() error {
 		time.Sleep(time.Second * 20)
 
 		var err error
-		connTest, err = sql.Open("postgres", fmt.Sprintf("postgres://postgres:secret@localhost:5433/GO_POSTGRES_GRAPHQL_DB?sslmode=disable"))
+		connTest, err = sql.Open("postgres", "postgres://postgres:secret@localhost:5433/GO_POSTGRES_GRAPHQL_DB?sslmode=disable")
 		if err != nil {
 			return err
 		}
 		return connTest.Ping()
 	}); poolErr != nil {
+		// コンテナ停止
+		closeContainer()
 		log.Fatalf("Could not connect to docker: %s", poolErr)
 		return poolErr
 	}
