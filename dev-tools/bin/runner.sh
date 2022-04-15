4ec225bed5765e7de75760d8a35d2436a1d9908e
yuki <yuki.onishi1129@gmail.com>
2022-04-16 02:06:10 +0900

    postgres migration修正

--- dev-tools/bin/runner.sh
+++ dev-tools/bin/runner.sh
@@ -16,7 +16,7 @@ case ${1} in
  ;;
 "db:migrate")
  echo  === db migrate start ===
- migrate -source file://app/database/migrations -database 'mysql://user:pass@tcp(127.0.0.1:3306)/GO_POSTGRES_GRAPHQL_DB' up
+ migrate -source file://app/database/migrations -database 'postgres://user:pass@localhost:5432/GO_POSTGRES_GRAPHQL_DB?sslmode=disable' up
  echo  === db migrate end ===
  ;;
 "db:seed")
@@ -34,7 +34,7 @@ case ${1} in
 "db:reset")
  # db:rollback
  echo  === db rollback start ===
- migrate -source file://app/database/migrations -database 'mysql://user:pass@tcp(127.0.0.1:3306)/GO_POSTGRES_GRAPHQL_DB' down
+ migrate -source file://app/database/migrations -database 'postgres://user:pass@localhost:5432/GO_POSTGRES_GRAPHQL_DB' down
  echo  === db rollback end ===
  # db:migrate
  echo  === db migrate start ===
